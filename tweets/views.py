dbf4913d9b98637f8679d360c342819a8eacfdda
Mike King <nyntofive@gmail.com>
2020-02-06 16:11:12 -0500

    vanilla JS crude setup

--- tweets/views.py
+++ tweets/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Tweet
 
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [ {"id": x.id, "content": x.content} for x in qs ]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
 def home_view(request, *args, **kwargs):
     print(f"Args:{args} | Kwargs: {kwargs}")
     return render(request, 'pages/home.html', context={}, status=200)
@@ -17,5 +26,5 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         data['content'] = obj.content
     except:
         data['message'] = 'Not Found!'
-        status = 404    
+        status = 404
     return JsonResponse(data, status=status)
