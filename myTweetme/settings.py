2382203aca96fb3bfd35463401d7e3b82006ab01
Mike King <nyntofive@gmail.com>
2020-02-13 16:10:06 -0500

    react build / django templateview

--- myTweetme/settings.py
+++ myTweetme/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
 import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,6 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+]
+STATIC_ROOT = os.path.join(BASE_DIR, "static-root")
+
 NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
 LOGIN_URL= 'login/'
 
